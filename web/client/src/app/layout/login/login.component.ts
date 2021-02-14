938c2939684cb3d4f2fcfe3c6dd84eb2430e1a9c
DAMM Nicolas <nicolas6578@gmail.com>
2021-02-14 01:37:42 +0100

    responsive header

--- web/client/src/app/layout/login/login.component.ts
+++ web/client/src/app/layout/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {NgForm} from '@angular/forms';
+import {faSignInAlt, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,19 @@ import {NgForm} from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   connected: boolean;
+  faSignInAlt = faSignInAlt;
+  faSignOutAlt = faSignOutAlt;
+  loginMobile: boolean;
 
   constructor(private auth: AuthService) {}
 
   ngOnInit(): void {
+    this.loginMobile = false;
     this.auth.connected$.subscribe(value => this.connected = value);
   }
 
   onSubmit(form: NgForm): void {
-    this.auth.login(form.value.email, form.value.password);
+    this.auth.login(form.value.email, form.value.password).then(() => this.loginMobile = false);
   }
 
   logout(): void {
